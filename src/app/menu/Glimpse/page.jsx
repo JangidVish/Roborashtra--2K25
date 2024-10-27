2b209f271c8923c5ddb436e98ab81ee604e80cf8
trushant1912 <trushantjadhav1912@gmail.com>
2024-10-27 19:03:37 +0530

    Glimpse

--- src/app/menu/Glimpse/page.jsx
+++ src/app/menu/Glimpse/page.jsx
@@ -1,74 +1,358 @@
-import React from 'react'
+// import React from 'react'
+// import "./glimpse.css";
+// import Image from "next/image"
+// import GlimpseCard from '@/components/glimpseCard/glimpseCard';
+// import glimpse1 from "../../../images/BlazeTrophy.png"
+// import glimpse7 from "../../../images/glimpse1.png"
+// import glimpse2 from "@/images/Team.png"
+// import glimpse3 from "@/images/blazeTeam.png"
+// import glimpse4 from "@/images/Trophy.png"
+// import glimpse5 from "@/images/ResqBot.png"
+// import glimpse6 from "@/images/resqArena.png"
+// import glimpse8 from "@/images/Glimpse/Audience.png"
+// import glimpse9 from "@/images/Glimpse/Evaluation2.png"
+// import glimpse10 from "@/images/Glimpse/Evalution1.png"
+// import glimpse11 from "@/images/Glimpse/Guest.png"
+// import glimpse12 from "@/images/Glimpse/Inaguration.png"
+// import glimpse13 from "@/images/Glimpse/Lead.png"
+// import glimpse14 from "@/images/Glimpse/PrincipalMam.png"
+// import glimpse15 from "@/images/Glimpse/PrizeDistribution1.png"
+// import glimpse16 from "@/images/Glimpse/Prizedistribution2.png"
+// import glimpse17 from "@/images/Glimpse/Prizesholding.png"
+// import glimpse18 from "@/images/Glimpse/Ribbin.png";
+// import glimpse19 from "@/images/Glimpse/check.png";
+// import { FaBold } from 'react-icons/fa';
+
+
+
+
+
+
+
+
+// function gallery() {
+//   return (
+
+//     <div className='gallery_wrapper'>
+      
+//         <br></br>
+//         <h1 className='justify-center text-center mt-[50px]'>Past Winner</h1>
+//         <h3>Cam Warrior</h3>
+//         <div className='imgcam'>
+//           <div>
+//             <GlimpseCard imgUrl={glimpse7} />
+//             <p className='text-center font-[aroma]'>Position: First Winner</p>
+//             <p className='text-center font-[aroma] '>Team : PHOENIX</p>
+//           </div>
+//           <div>
+//             <GlimpseCard imgUrl={glimpse7} />
+
+
+//             <p className='text-center font-[aroma]'>Position: Second Winner</p>
+//             <p className='text-center font-[aroma] '>Team : JMJM</p>
+
+//           </div>
+//           <div>
+//             <GlimpseCard imgUrl={glimpse7} />
+//             <p className='text-center font-[aroma]'>Position: Third Winner</p>
+//             <p className='text-center font-[aroma] '>Team : Vamrone</p>
+//           </div>
+
+
+
+//         </div>
+
+//         <div>
+//           <br></br>
+//           {/* <h1 className='justify-center text-center mt-[50px]'>Past Winner</h1> */}
+//           <h3>Blaze Maze</h3>
+//           <div className='imgcam'>
+//             <div>
+//               <GlimpseCard imgUrl={glimpse7} />
+//               <p className='text-center font-[aroma]'>Position: First Winner</p>
+//               <p className='text-center font-[aroma] '>Team : Vijay27</p>
+//             </div>
+//             <div>
+//               <GlimpseCard imgUrl={glimpse7} />
+
+
+//               <p className='text-center font-[aroma]'>Position: Second Winner</p>
+//               <p className='text-center font-[aroma] '>Team : Deepak Avhad</p>
+
+//             </div>
+//             <div>
+//               <GlimpseCard imgUrl={glimpse7} />
+//               <p className='text-center font-[aroma]'>Position: Third Winner</p>
+//               <p className='text-center font-[aroma] '>Team : Ranveer Rajput</p>
+//             </div>
+
+
+
+//           </div>
+//           <div>
+//             <br></br>
+
+//             <h3>ResQlympic</h3>
+//             <div className='imgcam'>
+//               <div>
+//                 <GlimpseCard imgUrl={glimpse7} />
+//                 <p className='text-center font-[aroma]'>Position: First Winner</p>
+//                 <p className='text-center font-[aroma] '>Team : Incronous</p>
+//               </div>
+//               <div>
+//                 <GlimpseCard imgUrl={glimpse7} />
+
+
+//                 <p className='text-center font-[aroma]'>Position: Second Winner</p>
+//                 <p className='text-center font-[aroma] '>Team : DRAIC</p>
+
+//               </div>
+//               <div>
+//                 <GlimpseCard imgUrl={glimpse7} />
+//                 <p className='text-center font-[aroma]'>Position: Third Winner</p>
+//                 <p className='text-center font-[aroma] '>Team : DRAIC-1</p>
+//               </div>
+              
+
+
+
+
+//             </div>
+//             <div>
+//             <br></br>
+
+//             <h3>Robofest Senior</h3>
+//             <div className='imgcam'>
+//               <div>
+//                 <GlimpseCard imgUrl={glimpse7} />
+//                 <p className='text-center font-[aroma]'>Position: First Winner</p>
+//                 <p className='text-center font-[aroma] '>Team : Rudra</p>
+//               </div>
+//               <div>
+//                 <GlimpseCard imgUrl={glimpse7} />
+
+
+//                 <p className='text-center font-[aroma]'>Position: Second Winner</p>
+//                 <p className='text-center font-[aroma] '>Team : Vinit Deshmukh</p>
+
+//               </div>
+//               <div>
+//                 <GlimpseCard imgUrl={glimpse7} />
+//                 <p className='text-center font-[aroma]'>Position: Third Winner</p>
+//                 <p className='text-center font-[aroma] '>Team : Robotics</p>
+//               </div>
+              
+//             </div>
+//             <div>
+//             <br></br>
+
+//             <h3>Robofest Junior</h3>
+//             <div className='imgcam'>
+//               <div>
+//                 <GlimpseCard imgUrl={glimpse7} />
+//                 <p className='text-center font-[aroma]'>Position: First Winner</p>
+//                 <p className='text-center font-[aroma] '>Team : Ranveer Rajput</p>
+//               </div>
+//               <div>
+//                 <GlimpseCard imgUrl={glimpse7} />
+
+
+//                 <p className='text-center font-[aroma]'>Position: Second Winner</p>
+//                 <p className='text-center font-[aroma] '>Team : Hi-fy</p>
+
+//               </div>
+//               <div>
+//                 <GlimpseCard imgUrl={glimpse7} />
+//                 <p className='text-center font-[aroma]'>Position: Third Winner</p>
+//                 <p className='text-center font-[aroma] '>Team : Edge Gaurdian</p>
+//               </div>
+              
+
+
+
+
+//             </div>
+
+
+
+
+//           </div>
+//           <br></br>
+//           <h1 >Our Gallery</h1>
+//           <div className="imgs">
+//             <GlimpseCard imgUrl={glimpse7} />
+//             <GlimpseCard imgUrl={glimpse2} />
+//             <GlimpseCard imgUrl={glimpse3} />
+//             <GlimpseCard imgUrl={glimpse1} />
+//             <GlimpseCard imgUrl={glimpse4} />
+//             <GlimpseCard imgUrl={glimpse5} />
+//             <GlimpseCard imgUrl={glimpse6} />
+//             <GlimpseCard imgUrl={glimpse8} />
+//             <GlimpseCard imgUrl={glimpse9} />
+//             <GlimpseCard imgUrl={glimpse10} />
+//             <GlimpseCard imgUrl={glimpse11} />
+//             <GlimpseCard imgUrl={glimpse12} />
+//             <GlimpseCard imgUrl={glimpse13} />
+//             <GlimpseCard imgUrl={glimpse14} />
+//             <GlimpseCard imgUrl={glimpse15} />
+//             <GlimpseCard imgUrl={glimpse15} />
+//             <GlimpseCard imgUrl={glimpse16} />
+//             <GlimpseCard imgUrl={glimpse17} />
+//             <GlimpseCard imgUrl={glimpse18} />
+//             <GlimpseCard imgUrl={glimpse19} />
+
+
+
+
+//             {/* <img src={img1} alt="" />
+//             <img src={img2} alt="" />
+//             <img src={img3} alt="" />
+//             <img src={img4} alt="" />
+//             <img src={img5} alt="" />
+//             <img src={teamMeeting} alt="" />
+//             <img src={teamMeeting} alt="" /> */}
+//           </div>
+//       </div>
+//     </div>
+//         );
+// };
+
+//         export default gallery;
+import React from 'react';
 import "./glimpse.css";
-import Image from "next/image"
+import Image from "next/image";
 import GlimpseCard from '@/components/glimpseCard/glimpseCard';
-import glimpse1 from "../../../images/BlazeTrophy.png"
-import glimpse7 from "../../../images/glimpse1.png"
-import glimpse2 from "@/images/Team.png"
-import glimpse3 from "@/images/blazeTeam.png"
-import glimpse4 from "@/images/Trophy.png"
-import glimpse5 from "@/images/ResqBot.png"
-import glimpse6 from "@/images/resqArena.png"
-import glimpse8 from  "@/images/Glimpse/Audience.png"
-import glimpse9 from  "@/images/Glimpse/Evaluation2.png"
-import glimpse10 from  "@/images/Glimpse/Evalution1.png"
-import glimpse11 from  "@/images/Glimpse/Guest.png"
-import glimpse12 from  "@/images/Glimpse/Inaguration.png"
-import glimpse13 from  "@/images/Glimpse/Lead.png"
-import glimpse14 from  "@/images/Glimpse/PrincipalMam.png"
-import glimpse15 from  "@/images/Glimpse/PrizeDistribution1.png"
-import glimpse16 from  "@/images/Glimpse/Prizedistribution2.png"
-import glimpse17 from  "@/images/Glimpse/Prizesholding.png"
-import glimpse18 from  "@/images/Glimpse/Ribbin.png";
+
+// Importing images
+import glimpse1 from "../../../images/BlazeTrophy.png";
+import glimpse7 from "../../../images/glimpse1.png";
+import glimpse2 from "@/images/Team.png";
+import glimpse3 from "@/images/blazeTeam.png";
+import glimpse4 from "@/images/Trophy.png";
+import glimpse5 from "@/images/ResqBot.png";
+import glimpse6 from "@/images/resqArena.png";
+import glimpse8 from "@/images/Glimpse/Audience.png";
+import glimpse9 from "@/images/Glimpse/Evaluation2.png";
+import glimpse10 from "@/images/Glimpse/Evalution1.png";
+import glimpse11 from "@/images/Glimpse/Guest.png";
+import glimpse12 from "@/images/Glimpse/Inaguration.png";
+import glimpse13 from "@/images/Glimpse/Lead.png";
+import glimpse14 from "@/images/Glimpse/PrincipalMam.png";
+import glimpse15 from "@/images/Glimpse/PrizeDistribution1.png";
+import glimpse16 from "@/images/Glimpse/Prizedistribution2.png";
+import glimpse17 from "@/images/Glimpse/Prizesholding.png";
+import glimpse18 from "@/images/Glimpse/Ribbin.png";
 import glimpse19 from "@/images/Glimpse/check.png";
 
+// Reusable component for winner section
+const WinnerSection = ({ title, winners }) => (
+  <div>
+    <h3>{title}</h3>
+    <div className='imgcam'>
+      {winners.map((winner, index) => (
+        <div key={index}>
+          <GlimpseCard imgUrl={glimpse7} />
+          <p className='text-center font-[aroma]'>Position: {winner.position}</p>
+          <p className='text-center font-[aroma] '>Team: {winner.team}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 
 
+const Gallery = () => {
+  const institute = [
+    { instituteName: "Pimpri Chinchwad College of Engineering and Research" },
+    { instituteName: "Pimpri Chinchwad College of Engineering" },
+    { instituteName: "AISSMS College of Engineering" },
+    { instituteName: "Sinhgad College of Engineering" },
+    { instituteName: "MIT College of Engineering" },
+  ]
 
+  const instituteList = institute.map(eachinstitute =>
+    <div className='list-container bg-zinc-800 hover:bg-zinc-600 rounded-lg font-semibold'>{eachinstitute.instituteName}</div>
 
-
-
-function gallery() {
+  )
+  
   return (
     <div className='gallery_wrapper'>
-        <br></br>
-        <h1 >Our Gallery</h1>
-        <div className="imgs">
-        <GlimpseCard imgUrl={glimpse7}/>
-          <GlimpseCard imgUrl={glimpse2}/>
-          <GlimpseCard imgUrl={glimpse3}/>
-          <GlimpseCard  imgUrl={glimpse1}/>
-          <GlimpseCard imgUrl={glimpse4}/>
-          <GlimpseCard imgUrl={glimpse5}/>
-          <GlimpseCard imgUrl={glimpse6}/>
-          <GlimpseCard imgUrl={glimpse8}/>
-          <GlimpseCard imgUrl={glimpse9}/>
-          <GlimpseCard imgUrl={glimpse10}/>
-          <GlimpseCard imgUrl={glimpse11}/>
-          <GlimpseCard imgUrl={glimpse12}/>
-          <GlimpseCard imgUrl={glimpse13}/>
-          <GlimpseCard imgUrl={glimpse14}/>
-          <GlimpseCard imgUrl={glimpse15}/>
-          <GlimpseCard imgUrl={glimpse15}/>
-          <GlimpseCard imgUrl={glimpse16}/>
-          <GlimpseCard imgUrl={glimpse17}/>
-          <GlimpseCard imgUrl={glimpse18}/>
-          <GlimpseCard imgUrl={glimpse19}/>
+      <h1 className='justify-center text-center mt-[50px]'>Past Winner</h1>
+      
+      <WinnerSection
+        title="Cam Warrior"
+        winners={[
+          { position: "First Winner", team: "PHOENIX" },
+          { position: "Second Winner", team: "JMJM" },
+          { position: "Third Winner", team: "Vamrone" },
+        ]}
+      />
+      
+      <WinnerSection
+        title="Blaze Maze"
+        winners={[
+          { position: "First Winner", team: "Vijay27" },
+          { position: "Second Winner", team: "Deepak Avhad" },
+          { position: "Third Winner", team: "Ranveer Rajput" },
+        ]}
+      />
+      
+      <WinnerSection
+        title="ResQlympic"
+        winners={[
+          { position: "First Winner", team: "Incronous" },
+          { position: "Second Winner", team: "DRAIC" },
+          { position: "Third Winner", team: "DRAIC-1" },
+        ]}
+      />
+      
+      <WinnerSection
+        title="Robofest Senior"
+        winners={[
+          { position: "First Winner", team: "Rudra" },
+          { position: "Second Winner", team: "Vinit Deshmukh" },
+          { position: "Third Winner", team: "Robotics" },
+        ]}
+      />
+      
+      <WinnerSection
+        title="Robofest Junior"
+        winners={[
+          { position: "First Winner", team: "Ranveer Rajput" },
+          { position: "Second Winner", team: "Hi-fy" },
+          { position: "Third Winner", team: "Edge Guardian" },
+        ]}
+      />
+
+      <h1>Our Gallery</h1>
+      <div className="imgs">
+        {[glimpse7, glimpse2, glimpse3, glimpse1, glimpse4, glimpse5, glimpse6, glimpse8, glimpse9, glimpse10, glimpse11, glimpse12, glimpse13, glimpse14, glimpse15, glimpse16, glimpse17, glimpse18, glimpse19].map((imgUrl, index) => (
+          <GlimpseCard key={index} imgUrl={imgUrl} />
+        ))}
+      </div>
+      <div className='main-container '>
+      <div className='past-winner-container mt-[100px]'>
+      {/* <h1 className='institute-winner text-center font-bold p-[20px]'>Past Winner</h1> */}
+      <div className=' '  >
+      
+      </div>
+
+      </div>
 
 
+      <div className='past-institute-container'>
+      <h1 className='institute-winner text-center font-bold'>Past Institutes participated</h1>
+      <div className='inner-container-list flex flex-col'>
+        {instituteList}
+      </div>
 
-    
-            {/* <img src={img1} alt="" />
-            <img src={img2} alt="" />
-            <img src={img3} alt="" />
-            <img src={img4} alt="" />
-            <img src={img5} alt="" />
-            <img src={teamMeeting} alt="" />
-            <img src={teamMeeting} alt="" /> */}
-        </div>
+      </div>
+     
+
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default gallery
+export default Gallery;
